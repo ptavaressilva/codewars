7a47aad6b45f31242a93e9723f2ea4fe5448cdbb
Pedro Tavares Silva <ptavaressilva@gmail.com>
2022-07-13 15:59:42 +0200

    next smaller - standardized file names

--- 6kyu_Convert_string_to_camel_case.py
+++ 6kyu_Convert_string_to_camel_case.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/517abf86da9663f1d2000003
+
 def to_camel_case(text):
     # Complete the method/function so that it converts dash/underscore delimited words into camel casing.
     # The first word within the output should be capitalized only if the original word was capitalized
