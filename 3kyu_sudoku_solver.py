42a7a658bfaacaf47272a597a967e3dfc3515506
Pedro Tavares Silva <ptavaressilva@gmail.com>
2022-07-14 20:26:51 +0000

    Runs but does not stop

--- 3kyu_sudoku_solver.py
+++ 3kyu_sudoku_solver.py
@@ -7,36 +7,59 @@ import time
 OFF = 0
 ON = 1
 HIGH = 2
-DEBUG = OFF
+DEBUG = ON
 
 
 class Board:
     def __init__(self, starting_board):
-        solution = starting_board
-        candidates = [[[1, 2, 3, 4, 5, 6, 7, 8, 9]
-                       for i in range(0, 9)] for j in range(0, 9)]
+        self.solution = starting_board
+        self.candidates = [[[1, 2, 3, 4, 5, 6, 7, 8, 9]
+                            for i in range(0, 9)] for j in range(0, 9)]
 
     def not_solved(self):
+
+        if DEBUG >= ON:
+            print('Entered not_solved()')
+
         for i in range(0, 9):
-            for i in range(0, 9):
+            for j in range(0, 9):
                 if self.solution[i][j] == 0:  # something still missing
-                    return 0
-        return 1
+                    return True
+
+        return False
 
     def search_and_set(self, i, j):
 
-        # search in this line
-        for a in range(0, 9):
-            if self.solution[i][a] in self.candidates[i][j]:
-                # eliminate existing value from candidates
-                self.candidates[i][j].pop(self.solution[i][a])
+        if DEBUG >= ON:
+            print('Entered serach_and_set({},{})'.format(i, j))
 
         # search in this row
+        for a in range(0, 9):
+            if self.solution[i][a] in self.candidates[i][j]:
+
+                if DEBUG >= ON:
+                    print('>>   {} exists in candidates {}. Will remove it.'.format(
+                        self.solution[i][a], self.candidates[i][j]))
+
+                # eliminate existing value from candidates
+                self.candidates[i][j].pop(
+                    self.candidates[i][j].index(self.solution[i][a]))
+
+                if DEBUG >= ON:
+                    print('Found {} in line. Removing from '.format(
+                        self.solution[i][a], self.candidates[i][j]))
+
+        # search in this column
         if len(self.candidates[i][j]) > 1:  # value not yet found
             for a in range(0, 9):
                 if self.solution[a][j] in self.candidates[i][j]:
                     # eliminate existing value from candidates
-                    self.candidates[i][j].pop(self.solution[a][j])
+                    self.candidates[i][j].pop(
+                        self.candidates[i][j].index(self.solution[a][j]))
+
+                    if DEBUG >= ON:
+                        print('Found {} in row. Removing from candidates[{}][{}]'.format(
+                            self.solution[a][j], i, j))
 
         # search in this 3x3 space
         if len(self.candidates[i][j]) > 1:  # value not yet found
@@ -48,14 +71,29 @@ class Board:
                 for b in range(0, 3):
                     if self.solution[space[0] + a][space[1] + b] in self.candidates[i][j]:
                         # eliminate existing value from candidates
-                        self.candidates[i][j].pop(
-                            self.solution[space[0] + a][space[1] + b])
+                        self.candidates[i][j].pop(self.candidates[i][j].index(
+                            self.solution[space[0] + a][space[1] + b]))
+                    if DEBUG >= ON:
+                        print('Found {} in 3x3 space. Removing from candidates[{}][{}]'.format(
+                            self.solution[a][j], i, j))
+
+        if DEBUG >= ON:
+            print('Finished searching row, column and 3x3 space. Candidates = {}'.format(
+                self.candidates[i][j]))
 
         if len(self.candidates[i][j]) == 1:  # value found
             self.solution[i][i] == self.candidates[i][j][0]
 
+            if DEBUG >= ON:
+                print('The value for [{}][{}] if []'.format(
+                    i, j, self.solution[i][j]))
+
     def solve(self):
         '''Find the solution for the Sudoku puzzle'''
+
+        if DEBUG >= ON:
+            print('Entered solve()')
+
         while (self.not_solved()):
             for i in range(0, 9):
                 for j in range(0, 9):
@@ -67,6 +105,8 @@ def sudoku(puzzle):
     """return the solved puzzle as a 2d array of 9 x 9"""
 
     board = Board(puzzle)
+
+    board.solve()
 
     return puzzle
 
